7309d9b51196d3c9b1ceb74d4efc252354cc8385
CrazzyCreeper0000 <wesley_minecraft@yahoo.com>
2013-11-14 17:31:59 -0500

    LOE_0.1

---
+++ LOE_0.1/Code/landofEvar.java
@@ -0,0 +1,19 @@
+package LOE_0.1.code;
+
+import net.minecraftforge.common.EnumHelper;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+@Mod(modid = "landofEvar", name = "Land Of Evar", version = "0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class landofEvar {
+
+	@Init
+	public void load(FMLInitializationEvent event) {
+
+	}
+}
